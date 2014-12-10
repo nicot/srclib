2891434048d08f773f34e559ea5cc5d219852a24
Quinn Slack <qslack@qslack.com>
2014-12-10 12:23:12 -0800

    delta authors

--- src/delta_cmd.go
+++ src/delta_cmd.go
@@ -26,6 +26,15 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	_, err = deltaGroup.AddCommand("authors",
+		"list authors that changed between commits",
+		"The `src delta authors` subcommand lists authors whose code was modified between any 2 commits.",
+		&deltaAuthorsCmd,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 type DeltaCmd struct{}
@@ -36,9 +45,13 @@ func (c *DeltaCmd) Execute(args []string) error {
 	return nil
 }
 
-type DeltaDefsCmd struct {
+type DeltaCmdCommon struct {
 	Base string `short:"f" long:"from" description:"base commit" required:"yes"`
 	Head string `short:"t" long:"to" description:"head commit" default:"master"`
+}
+
+type DeltaDefsCmd struct {
+	DeltaCmdCommon
 
 	Stat bool `long:"stat" description:"show statistics (# added/changed/removed)"`
 }
@@ -46,40 +59,12 @@ type DeltaDefsCmd struct {
 var deltaDefsCmd DeltaDefsCmd
 
 func (c *DeltaDefsCmd) Execute(args []string) error {
+	_, ds, err := getDelta(c.DeltaCmdCommon)
+	if err != nil {
+		return err
+	}
+
 	cl := NewAPIClientWithAuthIfPresent()
-
-	repo, err := OpenRepo(".")
-	if err != nil {
-		return err
-	}
-
-	ds := sourcegraph.DeltaSpec{
-		Base: sourcegraph.RepoRevSpec{RepoSpec: repo.RepoRevSpec().RepoSpec, Rev: c.Base},
-		Head: sourcegraph.RepoRevSpec{RepoSpec: repo.RepoRevSpec().RepoSpec, Rev: c.Head},
-	}
-
-	delta, _, err := cl.Deltas.Get(ds, nil)
-	if err != nil {
-		return err
-	}
-
-	if GlobalOpt.Verbose {
-		log.Printf("# Resolved delta:")
-		buildStr := func(b *sourcegraph.Build) string {
-			if b == nil {
-				return "(none)"
-			}
-			if b.EndedAt.Valid && b.Success {
-				return fmt.Sprintf("%d (finished %s ago)", b.BID, time.Since(b.EndedAt.Time))
-			}
-			return fmt.Sprintf("%d (not ready)", b.BID)
-		}
-		log.Printf("# Base: %s@%s, build %s", delta.Base.RepoSpec.URI, delta.Base.CommitID, buildStr(delta.BaseBuild))
-		log.Printf("# Head: %s@%s, build %s", delta.Head.RepoSpec.URI, delta.Head.CommitID, buildStr(delta.HeadBuild))
-	}
-
-	fmt.Printf(fade("# From %s..%s\n"), delta.Base.CommitID, delta.Head.CommitID)
-
 	deltaDefs, _, err := cl.Deltas.ListDefs(ds, nil)
 	if err != nil {
 		return err
@@ -106,6 +91,80 @@ func (c *DeltaDefsCmd) Execute(args []string) error {
 	}
 
 	return nil
+}
+
+type DeltaAuthorsCmd struct {
+	DeltaCmdCommon
+}
+
+var deltaAuthorsCmd DeltaAuthorsCmd
+
+func (c *DeltaAuthorsCmd) Execute(args []string) error {
+	_, ds, err := getDelta(c.DeltaCmdCommon)
+	if err != nil {
+		return err
+	}
+
+	cl := NewAPIClientWithAuthIfPresent()
+	deltaAuthors, _, err := cl.Deltas.ListAffectedAuthors(ds, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, deltaAuthor := range deltaAuthors {
+		fmt.Printf("%s contributed to the following changed/deleted definitions:\n", bold(cyan(fmtDeltaPerson(&deltaAuthor.Person))))
+		for _, def := range deltaAuthor.Defs {
+			fmt.Printf("    %s\n", fmtDeltaDefName(def))
+		}
+		fmt.Println()
+	}
+
+	return nil
+}
+
+func getDelta(c DeltaCmdCommon) (*sourcegraph.Delta, sourcegraph.DeltaSpec, error) {
+	repo, err := OpenRepo(".")
+	if err != nil {
+		return nil, sourcegraph.DeltaSpec{}, err
+	}
+
+	ds := sourcegraph.DeltaSpec{
+		Base: sourcegraph.RepoRevSpec{RepoSpec: repo.RepoRevSpec().RepoSpec, Rev: c.Base},
+		Head: sourcegraph.RepoRevSpec{RepoSpec: repo.RepoRevSpec().RepoSpec, Rev: c.Head},
+	}
+
+	cl := NewAPIClientWithAuthIfPresent()
+	delta, _, err := cl.Deltas.Get(ds, nil)
+	if err != nil {
+		return nil, ds, err
+	}
+
+	if GlobalOpt.Verbose {
+		log.Printf("# Resolved delta:")
+		buildStr := func(b *sourcegraph.Build) string {
+			if b == nil {
+				return "(none)"
+			}
+			if b.EndedAt.Valid && b.Success {
+				return fmt.Sprintf("%d (finished %s ago)", b.BID, time.Since(b.EndedAt.Time))
+			}
+			return fmt.Sprintf("%d (not ready)", b.BID)
+		}
+		log.Printf("# Base: %s@%s, build %s", delta.Base.RepoSpec.URI, delta.Base.CommitID, buildStr(delta.BaseBuild))
+		log.Printf("# Head: %s@%s, build %s", delta.Head.RepoSpec.URI, delta.Head.CommitID, buildStr(delta.HeadBuild))
+	}
+
+	return delta, ds, nil
+}
+
+func fmtDeltaPerson(p *sourcegraph.Person) string {
+	if p.User.Transient {
+		return p.User.Name
+	}
+	if p.User.Name == "" {
+		return p.User.Login
+	}
+	return fmt.Sprintf("%s (%s)", p.User.Login, p.User.Name)
 }
 
 func fmtDeltaDefName(d *sourcegraph.Def) string {
