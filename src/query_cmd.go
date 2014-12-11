ea2e29bf89bfecb301b29d8d42a949f7c0071eda
Quinn Slack <qslack@qslack.com>
2014-12-11 05:56:42 -0800

    continuous search

--- src/query_cmd.go
+++ src/query_cmd.go
@@ -5,9 +5,17 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"os/signal"
+	"path"
+	"path/filepath"
 	"strings"
+	"sync"
+	"syscall"
 
+	"github.com/bobappleyard/readline"
 	"github.com/kr/fs"
 	"github.com/kr/text"
 
@@ -15,6 +23,7 @@ import (
 	"sourcegraph.com/sourcegraph/srclib/buildstore"
 	"sourcegraph.com/sourcegraph/srclib/dep"
 	"sourcegraph.com/sourcegraph/srclib/graph"
+	"sourcegraph.com/sourcegraph/srclib/util"
 	"sourcegraph.com/sourcegraph/vcsstore/vcsclient"
 )
 
@@ -37,6 +46,7 @@ type QueryCmd struct {
 	AddDeps      bool   `long:"add-deps" description:"add dependency repos to remote if not present (specify this if you get a 'repo not found' error)"`
 	RepoURI      string `short:"r" long:"repo" description:"repository URI (defaults to current VCS repository 'srclib' or 'origin' remote URL)" required:"yes"`
 	CommitID     string `short:"c" long:"commit" description:"commit ID of repository to search (defaults to current repository's commit if build data is present, otherwise newest built remote commit on default branch)"`
+	Def          bool   `short:"d" long:"def" description:"show definitions"`
 	Refs         int    `short:"x" long:"refs" description:"show this many references/examples"`
 	ContextLines int    `short:"L" long:"context-lines" description:"number of surrounding context lines to show in ref/example code snippets" default:"3"`
 }
@@ -66,6 +76,7 @@ func (c *QueryCmd) Execute(args []string) error {
 	depSuffix := buildstore.DataTypeSuffix([]*dep.ResolvedDep{})
 	w := fs.WalkFS(".", commitFS)
 	seenDepURI := map[string]bool{}
+	depTargets := map[dep.ResolvedTarget]struct{}{}
 	for w.Step() {
 		depfile := w.Path()
 		if strings.HasSuffix(depfile, depSuffix) {
@@ -81,6 +92,7 @@ func (c *QueryCmd) Execute(args []string) error {
 			for _, d := range deps {
 				if d.Target != nil && d.Target.ToRepoCloneURL != "" {
 					depURI := graph.MakeURI(d.Target.ToRepoCloneURL)
+					depTargets[*d.Target] = struct{}{}
 					if !seenDepURI[depURI] {
 						repoAndDepURIs = append(repoAndDepURIs, depURI)
 						seenDepURI[depURI] = true
@@ -132,8 +144,140 @@ func (c *QueryCmd) Execute(args []string) error {
 		}
 		fmt.Fprint(&queryBuf, " ")
 	}
-	fmt.Fprint(&queryBuf, strings.Join(args, " "))
-	query := queryBuf.String()
+	queryConstraints := queryBuf.String()
+
+	if len(args) > 0 {
+		queryString := strings.Join(args, " ")
+		return query(c, cl, queryConstraints, queryString)
+	}
+
+	// Readline completion from names of defs in all relevant repos.
+	var (
+		comps   []string
+		compc   = make(chan string)
+		compsMu sync.Mutex
+	)
+	readline.Completer = func(query, ctx string) []string {
+		compsMu.Lock()
+		defer compsMu.Unlock()
+		var matches []string
+		for _, comp := range comps {
+			if strings.HasPrefix(strings.ToLower(comp), strings.ToLower(query)) {
+				matches = append(matches, comp)
+			}
+		}
+		return matches
+	}
+	go func() {
+		for comp := range compc {
+			compsMu.Lock()
+			comps = append(comps, comp)
+			compsMu.Unlock()
+		}
+	}()
+	for _, repoURI := range repoAndDepURIs {
+		compc <- path.Base(repoURI)
+	}
+	for depTarget := range depTargets {
+		go func(dt dep.ResolvedTarget) {
+			repoRevSpec := sourcegraph.RepoRevSpec{
+				RepoSpec: sourcegraph.RepoSpec{URI: graph.MakeURI(dt.ToRepoCloneURL)},
+				Rev:      dt.ToRevSpec,
+			}
+			b, _, err := cl.Repos.GetBuild(repoRevSpec, nil)
+			if err != nil || b == nil {
+				if GlobalOpt.Verbose {
+					log.Printf("Warning: unable to get build for %s (for query completion): %s.", dt.ToRepoCloneURL, err)
+				}
+				return
+			}
+			if b.LastSuccessful == nil {
+				if GlobalOpt.Verbose {
+					log.Printf("Warning: no successful builds for %s (for query completion).", dt.ToRepoCloneURL)
+				}
+				return
+			}
+
+			defs, _, err := cl.Defs.List(&sourcegraph.DefListOptions{
+				Repos:       []string{graph.MakeURI(dt.ToRepoCloneURL)},
+				CommitID:    b.LastSuccessful.CommitID,
+				UnitTypes:   []string{dt.ToUnitType},
+				Unit:        dt.ToUnit,
+				Exported:    true,
+				Sort:        "xrefs",
+				Direction:   "desc",
+				ListOptions: sourcegraph.ListOptions{PerPage: 500},
+			})
+			if err != nil {
+				if GlobalOpt.Verbose {
+					log.Printf("Warning: unable to list defs for %s (for query completion): %s.", dt.ToRepoCloneURL, err)
+				}
+				return
+			}
+			if GlobalOpt.Verbose {
+				log.Println("Got completions for", dt.ToRepoCloneURL, dt.ToUnitType)
+			}
+			for _, def := range defs {
+				compc <- def.Name
+				if def.FmtStrings != nil {
+					qname := def.FmtStrings.Name.DepQualified
+					if strings.Count(qname, ".") < 2 && !strings.Contains(qname, "(") {
+						// Only complete on simple selectors for now.
+						compc <- qname
+					}
+				}
+			}
+		}(depTarget)
+	}
+
+	defer readline.Cleanup()
+	histFile := filepath.Join(util.CurrentUserHomeDir(), ".src-query-history")
+	if err := readline.LoadHistory(histFile); err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	defer func() {
+		if err := readline.SaveHistory(histFile); err != nil {
+			log.Printf("Warning: unable to save query history to %s: %s.", histFile, err)
+		}
+	}()
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGINT)
+	readline.CatchSigint = true
+	errc := make(chan error)
+	done := make(chan struct{})
+	go func() {
+		for {
+			line, err := readline.String(cyan("✱") + " ")
+			if err != nil {
+				if err == io.EOF {
+					close(done)
+				} else {
+					errc <- err
+				}
+				return
+			}
+			line = strings.TrimSpace(line)
+			if line == "" {
+				continue
+			}
+			readline.AddHistory(line)
+			if err := query(c, cl, queryConstraints, line); err != nil {
+				errc <- err
+			}
+		}
+	}()
+	select {
+	case <-sigint:
+		return nil
+	case err := <-errc:
+		return err
+	case <-done:
+	}
+	return nil
+}
+
+func query(c *QueryCmd, cl *sourcegraph.Client, queryConstraints, queryString string) error {
+	query := queryConstraints + " " + queryString
 	if GlobalOpt.Verbose {
 		log.Printf("# Query: %q", query)
 	}
@@ -141,7 +285,7 @@ func (c *QueryCmd) Execute(args []string) error {
 	res, _, err := cl.Search.Search(&sourcegraph.SearchOptions{
 		Query:       query,
 		Defs:        true,
-		ListOptions: sourcegraph.ListOptions{PerPage: 25},
+		ListOptions: sourcegraph.ListOptions{PerPage: 12},
 	})
 	if err != nil {
 		return err
@@ -200,11 +344,45 @@ func (c *QueryCmd) Execute(args []string) error {
 
 		fmt.Printf("%-50s %s\n", fade(src), fade(stat))
 
+		if c.Def {
+			// Show definition.
+			entrySpec := sourcegraph.TreeEntrySpec{
+				RepoRev: sourcegraph.RepoRevSpec{
+					RepoSpec: sourcegraph.RepoSpec{URI: def.Repo},
+					Rev:      def.CommitID,
+					CommitID: def.CommitID,
+				},
+				Path: def.File,
+			}
+			opt := &sourcegraph.RepoTreeGetOptions{
+				GetFileOptions: vcsclient.GetFileOptions{
+					FileRange: vcsclient.FileRange{StartByte: def.DefStart, EndByte: def.DefEnd},
+					FullLines: true,
+				},
+			}
+			entry, _, err := cl.RepoTree.Get(entrySpec, opt)
+			if err == nil {
+				entry.Contents = bytes.Replace(entry.Contents, []byte(def.Name), []byte(bold(yellow(def.Name))), -1)
+				fmt.Println(text.Indent(string(entry.Contents), "  "))
+			} else {
+				log.Printf("Error fetching def %s in %s. Skipping.", def.Path, def.Repo)
+				if GlobalOpt.Verbose {
+					log.Println(err)
+				}
+			}
+			fmt.Println()
+		}
+
 		if c.Refs > 0 {
 			opt := &sourcegraph.DefListRefsOptions{ListOptions: sourcegraph.ListOptions{PerPage: c.Refs}}
 			xs, _, err := cl.Defs.ListRefs(def.DefSpec(), opt)
 			if err != nil {
-				return err
+				log.Printf("Error listing refs for %s in %s unit %s. Skipping.", def.Path, def.Repo, def.Unit)
+				if GlobalOpt.Verbose {
+					log.Println(err)
+				}
+				log.Println()
+				continue
 			}
 			fmt.Println()
 			for _, x := range xs {
@@ -230,6 +408,7 @@ func (c *QueryCmd) Execute(args []string) error {
 					if GlobalOpt.Verbose {
 						log.Println(err)
 					}
+					log.Println()
 					continue
 				}
 
